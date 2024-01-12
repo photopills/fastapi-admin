43ba8b308f5f67e2d33b951838ef82f951ae28e3
dbf <somnium@riseup.net>
2024-01-12 11:07:24 +0100

    Remove `jinja2.ext.autoescape` load
    Now is a built in ext

--- fastapi_admin/template.py
+++ fastapi_admin/template.py
@@ -14,8 +14,6 @@ templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 templates.env.globals["VERSION"] = VERSION
 templates.env.globals["NOW_YEAR"] = date.today().year
 templates.env.add_extension("jinja2.ext.i18n")
-templates.env.add_extension("jinja2.ext.autoescape")
-
 
 @pass_context
 def current_page_with_params(context: dict, params: dict):
